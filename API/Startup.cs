d19e3d2dfa20e7ec6f530288538582005f89aa52
unknown <vishwa.graphic@gmail.com>
2022-02-18 10:46:15 -0500

    changes for router and request

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
+using System;
 using API.Data;
+using API.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -27,19 +29,23 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
-            services.AddDbContext<StoreContext>(opt => {
+            services.AddDbContext<StoreContext>(opt =>
+            {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
             services.AddCors();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
@@ -48,9 +54,10 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(opt => {
-                    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-                }
+            app.UseCors(opt =>
+            {
+                opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            }
             );
 
             app.UseAuthorization();
